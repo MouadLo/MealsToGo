6725a5683f233d415047bebdbfd20698b3f5cc9b
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2022-01-23 06:53:46 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+	StyleSheet,
+	StatusBar,
+	Text,
+	View,
+	SafeAreaView,
+	Platform,
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<SafeAreaView style={styles.container}>
+				<View
+					style={{
+						backgroundColor: 'green',
+						padding: 16,
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<Text>Search</Text>
+				</View>
+				<View
+					style={{
+						flex: 1,
+						backgroundColor: 'red',
+						padding: 16,
+					}}
+				>
+					<Text>Item List</Text>
+				</View>
+			</SafeAreaView>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight,
+	},
 });
